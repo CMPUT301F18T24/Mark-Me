517dcc4e4bace4897c1e200d6cee26cf48ab72ce
Rizwan Qureshi <rizwan@ualberta.ca>
2018-11-01 14:16:12 -0600

    initial test implementation

--- app/src/androidTest/java/com/cybersix/markme/ProblemModelTest.java
+++ app/src/androidTest/java/com/cybersix/markme/ProblemModelTest.java
@@ -1,7 +1,24 @@
 package com.cybersix.markme;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ProblemModelTest {
+    @Test
+    public void addRecord() {
+        assertTrue(false);
+    }
 
+    @Test
+    public void removeRecord() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void getRecord() {
+        assertTrue(false);
+    }
+
+    public void 
 }
