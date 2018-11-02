f1d2adf213533b6803e549506d55d48373459922
curtis goud <cgoud@ualberta.ca>
2018-11-02 00:49:13 -0600

    Cleanup and BodyPart tests

--- app/src/androidTest/java/com/cybersix/markme/BodyLocationTest.java
+++ app/src/androidTest/java/com/cybersix/markme/BodyLocationTest.java
@@ -7,10 +7,20 @@ import static org.junit.Assert.*;
 public class BodyLocationTest {
 
     @Test
-    public void getBodyPart() {
+    public void testGetBodyPart() {
+        EBodyPart EBP = EBodyPart.ABDOMEN;
+        BodyLocation bl = new BodyLocation(EBP);
+        assertEquals(bl.getBodyPart(),EBodyPart.ABDOMEN);
+        EBodyPart getBP = bl.getBodyPart();
+        assertEquals(getBP,EBP);
     }
 
     @Test
-    public void setBodyPart() {
+    public void testSetBodyPart() {
+        BodyLocation bl = new BodyLocation(EBodyPart.ABDOMEN);
+        assertEquals(bl.getBodyPart(),EBodyPart.ABDOMEN);
+        EBodyPart newBP = EBodyPart.HEAD;
+        bl.setBodyPart(newBP);
+        assertEquals(newBP,bl.getBodyPart());
     }
 }
--- app/src/androidTest/java/com/cybersix/markme/EBodyPartTest.java
+++
@@ -1,5 +0,0 @@
-package com.cybersix.markme;
-
-public class EBodyPartTest {
-
-}
