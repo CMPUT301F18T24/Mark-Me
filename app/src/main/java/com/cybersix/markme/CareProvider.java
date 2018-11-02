55fca54d7fd1e3d54c5b780404f7e72572698594
curtis goud <cgoud@ualberta.ca>
2018-11-02 11:32:58 -0600

    CareProvider implementation and test cases

--- app/src/main/java/com/cybersix/markme/CareProvider.java
+++ app/src/main/java/com/cybersix/markme/CareProvider.java
@@ -1,8 +1,30 @@
 package com.cybersix.markme;
 
+import java.util.ArrayList;
+
 public class CareProvider extends UserModel {
-    private String name;
-    public CareProvider(String name){
-        this.name = name;
+    private ArrayList<Patient> patients;
+
+    public CareProvider(String username){
+        super();
+        super.setUserID(username);
+        patients = new ArrayList<Patient>();
+    }
+
+    public void addPatient(Patient p){
+        patients.add(p);
+    }
+
+    public Patient removePatient(Patient p){
+        patients.remove(p);
+        return p;
+    }
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    public boolean hasPatient(Patient p){
+        return patients.contains(p);
     }
 }
