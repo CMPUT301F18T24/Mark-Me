079a27e037b35ae81e997d146590f1e9ee799ec3
curtis goud <cgoud@ualberta.ca>
2018-11-09 13:35:02 -0700

    Added body activity and initialized some ENUM properties
    Beginning work on mapping Parts to screen for various screen sizes

--- app/src/main/java/com/cybersix/markme/BodyActivity.java
+++ app/src/main/java/com/cybersix/markme/BodyActivity.java
@@ -1,13 +1,49 @@
 package com.cybersix.markme;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BodyActivity extends AppCompatActivity {
+
+    private ImageView bodyView;
+    private DisplayMetrics dm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+        bodyView = (ImageView) findViewById(R.id.bodyView);
+        dm = getResources().getDisplayMetrics();
+        bodyView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if(action == MotionEvent.ACTION_UP){
+                    //float xdp = event.getX()*(160f/dpi); //Necessary calculation.. Scales X and Y to screen size
+                    //float ydp = event.getY()*(160f/dpi);
+                    int h = bodyView.getHeight();
+                    int w = bodyView.getWidth();
+                    Log.d("Size","h: " + h + " " + "w: " + w);
+
+                    float xdp = 0;
+                    float ydp = 0;
+
+                    Log.d("BODY TOUCH","X: " + xdp + " " + "Y: " + ydp);
+                    for(EBodyPart part : EBodyPart.values()){
+                        if(xdp <= part.getX2() && xdp>=part.getX1() && ydp<=part.getY2() && ydp>=part.getY1()){
+                            Log.d("BODY HIT",part.toString());
+                        }
+                    }
+                }
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/cybersix/markme/EBodyPart.java
+++ app/src/main/java/com/cybersix/markme/EBodyPart.java
@@ -1,18 +1,54 @@
 package com.cybersix.markme;
 
 public enum EBodyPart {
-    LEFTHAND,
-    LEFTARM,
-    LEFTLEG,
-    LEFTFOOT,
-    RIGHTHAND,
-    RIGHTARM,
-    RIGHTLEG,
-    RIGHTFOOT,
-    HEAD,
-    NECK,
-    CHEST,
-    ABDOMEN,
-    UPPERBACK,
-    LOWERBACK
+    LEFTHAND("Left Hand",276.86645,253.24219,300.39917,284.76562),
+    LEFTARM("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    LEFTLEG("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    LEFTFOOT("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    RIGHTHAND("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    RIGHTARM("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    RIGHTLEG("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    RIGHTFOOT("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    HEAD("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    NECK("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    CHEST("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    ABDOMEN("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    UPPERBACK("Left Hand",553.7329,531.4844,607.8076,560.5078),
+    LOWERBACK("Left Hand",553.7329,531.4844,607.8076,560.5078);
+
+    private final String name;
+    private final float x1;
+    private final float x2;
+    private final float y1;
+    private final float y2;
+
+    private EBodyPart(String name,double x1,double y1, double x2, double y2){
+        this.name=name;
+        this.x1=(float)x1;
+        this.x2=(float)x2;
+        this.y1=(float)y1;
+        this.y2=(float)y2;
+    }
+
+    public float getX1(){
+        return this.x1;
+    }
+
+    public float getX2(){
+        return this.x2;
+    }
+
+    public float getY1(){
+        return this.y1;
+    }
+
+    public float getY2(){
+        return this.y2;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+
+
 }
