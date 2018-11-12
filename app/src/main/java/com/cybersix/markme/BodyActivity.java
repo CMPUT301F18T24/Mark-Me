f1ecbf0a91f28ee7b251d810fcd25bc44a0c6a3f
curtis goud <cgoud@ualberta.ca>
2018-11-11 17:57:45 -0700

    Body rotation and location selection

--- app/src/main/java/com/cybersix/markme/BodyActivity.java
+++ app/src/main/java/com/cybersix/markme/BodyActivity.java
@@ -1,5 +1,6 @@
 package com.cybersix.markme;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -7,28 +8,74 @@ import android.graphics.Path;
 import android.graphics.Region;
 import android.graphics.drawable.VectorDrawable;
 import android.os.Debug;
+import android.support.constraint.ConstraintLayout;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import org.jetbrains.annotations.Nullable;
 
 public class BodyActivity extends AppCompatActivity {
 
+
+    private class PointView extends View{
+
+        private final float x;
+        private final float y;
+
+        public PointView(Context context, @Nullable AttributeSet ars, float x, float y){
+            super(context,ars);
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas){
+            super.onDraw(canvas);
+            Paint p = new Paint();
+            p.setColor(Color.RED);
+            canvas.drawCircle(x,y,8,p);
+        }
+
+
+
+    }
+
     private ImageView bodyView;
+    private PointView point;
+    private ImageButton rotateButton;
+    private ConstraintLayout bodyConstraintLayout;
     private DisplayMetrics dm;
+    private boolean frontFacing = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
         bodyView = (ImageView) findViewById(R.id.bodyView);
+        bodyConstraintLayout = (ConstraintLayout) findViewById(R.id.bodyConstraintLayout);
         dm = getResources().getDisplayMetrics();
+        rotateButton = (ImageButton) findViewById(R.id.rotateButton);
+
+        rotateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                reverse();
+            }
+
+        });
         bodyView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -46,8 +93,11 @@ public class BodyActivity extends AppCompatActivity {
                     for(EBodyPart part : EBodyPart.values()){
 
 
-                        if(xdp >= part.getP1().x && xdp<=part.getP2().x && ydp >= part.getP1().y && ydp <= part.getP2().y){
+                        if(xdp >= part.getP1().x && xdp<=part.getP2().x && ydp >= part.getP1().y && ydp <= part.getP2().y && part.getFace() == frontFacing){
                             Log.d("BODY HIT",part.toString());
+                            bodyConstraintLayout.removeView(point);
+                            point = new PointView(BodyActivity.this, null,event.getX(),event.getY());
+                            bodyConstraintLayout.addView(point);
                         }
                     }
                 }
@@ -56,5 +106,9 @@ public class BodyActivity extends AppCompatActivity {
         });
     }
 
+
+    private void reverse(){
+        frontFacing = !frontFacing;
+    }
 
 }
