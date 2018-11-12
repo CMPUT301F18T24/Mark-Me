afa181053e5162d253a9b60687ee36189fc51e79
Rizwan Qureshi <rizwan@ualberta.ca>
2018-11-12 16:14:56 -0700

    finished LiveCameraActivity class, may need to adjust for bodylocation overlays

--- app/src/main/java/com/cybersix/markme/LiveCameraActivity.java
+++ app/src/main/java/com/cybersix/markme/LiveCameraActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
-import android.widget.Button;
 
 import java.io.ByteArrayOutputStream;
 
