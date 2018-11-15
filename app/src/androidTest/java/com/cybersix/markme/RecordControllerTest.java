5c45d1e6c625389d482b8947a5781506d2a46047
Jose Ramirez <jose@ualberta.ca>
2018-11-14 22:13:48 -0700

    Added the record controller test with minimum tests

--- app/src/androidTest/java/com/cybersix/markme/RecordControllerTest.java
+++ app/src/androidTest/java/com/cybersix/markme/RecordControllerTest.java
@@ -1,21 +1,68 @@
 package com.cybersix.markme;
 
+import android.graphics.Bitmap;
+import android.location.Location;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class RecordControllerTest {
 
     @Test
     public void testCreateNewRecord() {
+        // this will ensure that the correct record is added to the record controller
+        String title = "testTitle";
+        String description = "testDescription";
+
+        RecordController testController = new RecordController();
+        testController.createNewRecord(title, description, null, null, null);
+        RecordModel record = testController.records.get(0);
+
+        // check that the record information has been saved
+        assertEquals(record.getTitle(), title);
+        assertEquals(record.getDescription(), description);
+    }
+
+    @Test
+    public void testEditRecord() {
+        // this will ensure that the correct record is added to the record controller
+        String title = "testTitle";
+        String description = "testDescription";
+
+        String titleEdit = "testTitle is edited";
+        String descriptionEdit = "testDescription is now edited";
+        String commentEdit = "This is a comment that is added to the record";
+        ArrayList<Bitmap> editPhotos = new ArrayList<Bitmap>(14);
+        BodyLocation editBodyLocation = new BodyLocation(EBodyPart.LEFTARM);
+        Location editLocation = new Location("testProviders");
+
+        RecordController testController = new RecordController();
+        testController.createNewRecord(title, description, null, null, null);
+
+        try {
+            // now we edit the created record
+            testController.editRecord(0, titleEdit, descriptionEdit, editPhotos, editBodyLocation,
+                    editLocation, commentEdit);
+            // get the record
+            RecordModel record = testController.records.get(0);
+            assertEquals(record.getTitle(), titleEdit);
+            assertEquals(record.getDescription(), descriptionEdit);
+            assertEquals(record.getComment(), commentEdit);
+            assertEquals(record.getPhotos(), editPhotos);
+            assertEquals(record.getBodyLocation(), editBodyLocation);
+            assertEquals(record.getMapLocation(), editLocation);
+        }
+        catch (Exception e) {
+            // not expected to get here
+            assert(false);
+        }
 
     }
 
     @Test
     public void testLoadRecordData() {
-
-    }
-
-    @Test
-    public void testEditRecord() {
 
     }
 
@@ -26,6 +73,6 @@ public class RecordControllerTest {
 
     @Test
     public void removeRecordData() {
-
+        // this will ensure that the correct record is removed
     }
 }
--- app/src/main/java/com/cybersix/markme/RecordController.java
+++ app/src/main/java/com/cybersix/markme/RecordController.java
@@ -15,6 +15,8 @@
 package com.cybersix.markme;
 
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -34,22 +36,108 @@ public class RecordController {
     public static RecordController getInstance() {
         if (instance == null) {
             instance = new RecordController();
-            uploadFakeData();
+            loadFakeData();
         }
         return instance;
     }
 
     // TODO: This function will be in charge of filling up some fake record data for the views
-    private static void uploadFakeData() {
+    private static void loadFakeData() {
         // This will fill up the list of records with fake records. There will be around 30 of them
+        for (int i = 0; i < 30; i++) {
+            String title = "Fake Record Title " + Integer.toString(i);
+            String description = "Fake record descriptions for title " + Integer.toString(i);
+            instance.createNewRecord(title, description, null, null, null);
+        }
     }
 
-    public void createNewRecord(String title, String description, ArrayList<Bitmap> photos,
+    /**
+     * This function will create a new record to add to the associated list of records to the controller
+     * @param title the title of the record
+     * @param description the description of the record
+     * @param photos the list of photos to add for the record (optional)
+     * @param bodyLocation the location on the body of the record (optional)
+     */
+    public void createNewRecord(String title, String description, ArrayList<Bitmap> photos, Location location,
                                 BodyLocation bodyLocation) {
         // This will need to be modified as soon as the gelocation stuff is handled
         //GeoLocationRecord location = null
         // expect bodylocation to be null sometimes
 
+        RecordModel record = new RecordModel(title, description);
+        if (photos != null) {
+            for (Bitmap photo : photos) {
+                try {
+                    record.addPhoto(photo);
+                }
+                catch (Exception e) {
+                    // display unable to save the photos
+                    String message = e.getMessage();
+                    return;
+                }
+            }
+        }
+        // set the body location
+        if (bodyLocation != null) {
+            record.setBodyLocation(bodyLocation);
+        }
+        // set the map location
+        if (location != null) {
+            record.setMapLocation(location);
+        }
+
+        // finally add the record to the record list
+        records.add(record);
+        Log.d("Jose_CreateRecord", "Record successfully created");
+
+    }
+
+    /**
+     * This function will edit the record in index i with any of the following fields
+     * @param index
+     * @param newTitle
+     * @param newDescription
+     * @param newPhotos
+     * @param newBodyLocation
+     * @param newLocation
+     * @param newComment
+     */
+    public void editRecord(int index, String newTitle, String newDescription, ArrayList<Bitmap> newPhotos,
+                           BodyLocation newBodyLocation, Location newLocation, String newComment) {
+
+        // edit the record information
+        RecordModel record = instance.records.get(index);
+        // set the new attributes
+        try {
+            if (newTitle != null || newTitle != "") {
+                record.setTitle(newTitle);
+            }
+            if (newDescription != null || newDescription != ""){
+                record.setDescription(newDescription);
+            }
+            if (newPhotos != null) {
+                for (Bitmap photo : newPhotos) {
+                    record.addPhoto(photo);
+                }
+            }
+            if (newBodyLocation != null) {
+                record.setBodyLocation(newBodyLocation);
+            }
+            if (newLocation != null) {
+                record.setMapLocation(newLocation);
+            }
+            if (newComment != null || newComment != "") {
+                record.setComment(newComment);
+            }
+            // we are done. Display a complete message
+            // TODO: implement a complete message for now put it in the log
+            Log.d("Jose_EditRecord","Successfully edited record");
+        }
+        catch (Exception e) {
+            // display an error for the description
+            String message = e.getMessage();
+            Log.d("Jose_EditRecord", message);
+        }
     }
 
     public ArrayList<RecordModel> loadRecordData(ProblemModel problemModel) {
@@ -57,9 +145,6 @@ public class RecordController {
         return problemModel.getRecords();
     }
 
-    public void editRecord(int index, RecordModel editRecord) {
-
-    }
 
     public void saveRecordData(ArrayList<RecordModel> records) {
 
