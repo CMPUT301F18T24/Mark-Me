1f5384bfffb8aaa53199395b223e89c0e3901b75
Dorsa Nahid <31228267+dorsMaster@users.noreply.github.com>
2018-11-20 02:14:20 -0700

    Update ProblemActivityCreation.java

--- app/src/main/java/com/cybersix/markme/ProblemActivityCreation.java
+++ app/src/main/java/com/cybersix/markme/ProblemActivityCreation.java
@@ -101,15 +101,13 @@ public class ProblemActivityCreation extends AppCompatActivity {
             }
         });
         AlertDialog dialog = builder.create();
-        switch (consistency){
-            case "d" :
-                scheduleNotification(this, 86400000, 12);
-            case "b" :
-                scheduleNotification(this, 172800000, 12);
-            case "w" :
-                scheduleNotification(this, 604800000, 12);
-            default:
-                break;
+        if(consistency.equalsIgnoreCase("d")){
+            scheduleNotification(this, 86400000, 12);
+        }else if(consistency.equalsIgnoreCase("b")){
+            scheduleNotification(this, 172800000, 12);
+
+        }else if(consistency.equalsIgnoreCase("w")){
+            scheduleNotification(this, 604800000, 12);
         }
         dialog.show();
 
